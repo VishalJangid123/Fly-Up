408e26e999a7f659246012c3b985bbe381847081
Vishal <vishaljangid2@outlook.com>
2021-01-26 22:47:55 +0700

    Swipe implement / Main menu / Theme Start / Assest added

--- Fly-Up/Assets/Script/GameManager.cs
+++ Fly-Up/Assets/Script/GameManager.cs
@@ -1,6 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Linq;
+
+public enum Level
+{
+    EASY,
+    HARD
+}
 
 
 public class GameManager : Singleton<GameManager>
@@ -26,10 +34,88 @@ public class GameManager : Singleton<GameManager>
     // get the audiosource
     [HideInInspector]
     public AudioSource _audioSource;
+
+    private List<Controller> _controllers;
+    private List<Controller> _levels;
+    public Level _selectedLevel;
+
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
 
+        _controllers = FindObjectsOfType<Controller>().ToList();
+        _levels = new List<Controller>();
+
+        foreach ( Controller i in _controllers)
+        {
+            if (i._desired_level == Level.EASY || i._desired_level == Level.HARD)
+            {
+                _levels.Add(i);
+            }
+        }
+
+        // check for playerpref
+        if (PlayerPrefs.HasKey("level"))
+        {
+            if (PlayerPrefs.GetInt("level") == (int)Level.EASY)
+            {
+                _selectedLevel = Level.EASY;
+            }
+            else if(PlayerPrefs.GetInt("level") == (int)Level.HARD)
+            {
+                _selectedLevel = Level.HARD;
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("level", (int)Level.EASY);
+            _selectedLevel = Level.EASY;
+
+        }
+
+        changeSelectedLevel(_selectedLevel);
+
+        SetDifficultyLevel();
+        
     }
+
+    void print()
+    {
+        Debug.Log(PlayerPrefs.GetInt("level"));
+    }
+
+    public void changeSelectedLevel(Level _level)
+    {
+
+        foreach (Controller level in _levels)
+        {
+            if( level._desired_level == _level)
+            {
+                level.GetComponent<Lean.Gui.LeanToggle>().On = true;
+            }
+            else
+            {
+                level.GetComponent<Lean.Gui.LeanToggle>().On = false;
+
+            }
+        }
+
+        _selectedLevel = _level;
+
+    }
+
+    void SetDifficultyLevel()
+    {
+        if(_selectedLevel == Level.EASY)
+        {
+
+        }
+        else if(_selectedLevel == Level.HARD)
+        {
+
+        }
+    }
+
+
 }
 
---
+++ Fly-Up/Assets/SettingsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onLevelToggleCliked(int level)
+    {
+        Level _level = Level.EASY;
+
+        switch(level)
+        {
+            case 0: _level = Level.EASY;break; 
+            case 1: _level = Level.HARD; break;
+            
+        }
+
+        Debug.Log(_level);
+        // change diffculty level
+        PlayerPrefs.SetInt("level", (int)_level);
+
+        GameManager.instance.changeSelectedLevel(_level);
+    }
+}
